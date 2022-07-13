2acdc6d6fda472560aec0f62a772e38c06d6b45a
moburo101 <moburoarah@gmail.com>
2022-07-13 21:44:50 +0100

    reponsive changes

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-var icon = document.querySelector(".icon");
+var burger = document.querySelector(".burger");
+var nav = document.querySelector(".nav-items");
+var items = document.querySelector(".nav-items a")
 
-icon.addEventListener("click", function(){
+burger.addEventListener("click", function(){
+    nav.style.display = "block";
     navBar()
 });
 
 function navBar(){
     var nav = document.getElementById("navItems");
     if(nav.className === "nav-items"){
-        nav.classList.add("responsive");
+        nav.classList.add("res-nav-items");
     } else{
         nav.className = "nav-items";
     }
